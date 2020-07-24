7c7fe5ea126266904c83827f59a8d85ad660f618
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-23 21:15:04 -0700

    Updates to JS functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,64 @@
-// Business Logic
+$(document).ready(function() {
+  $("#neighborhood").submit(function(event) {
+    event.preventDefault();
+    let number = parseInt($('input#number').val());
 
-// Computer Logic
+    let roboArray = [];
+
+  function numberList() {
+    let roboArray = [];
+    for (let i = 0; i <= number; i += 1) {
+      roboArray.push(i);
+      console.log(number);
+  };
+  return numberList;
+    
+}
+
+    $("#output").text(roboArray);
+    $("#answer").show(roboArray);
+
+  });
+});
+
+
+
+// function startThis() {
+//   let numberList = [] {
+//     for (let index = 0; index <= userNumber; index += 1) {
+//       numberList.push(index);
+//     };
+//     return numberList;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("#neighborhood").submit(function(event) {
+//     event.preventDefault();
+//     let number = parseInt($('input#number').val());
+
+//     let roboArray = [];
+
+//     for (let i = 0; i < number.length; i++) {
+//       roboArray.push(number[i]);
+//     }
+//     // roboArray.push(number);
+//     console.log(roboArray);
+
+//     $("#output").text(roboArray);
+//     $("#answer").show(roboArray);
+
+//   });
+// });
